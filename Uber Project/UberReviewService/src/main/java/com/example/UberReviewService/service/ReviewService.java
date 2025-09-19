96354667ad735b07f6c3cc4c14f90d5c21152507
Ashutosh <ashutoshmaddheshiya979@gmail.com>
2025-09-19 23:20:01 +0530

    new Database Migrations and Controller added

--- Uber Project/UberReviewService/src/main/java/com/example/UberReviewService/service/ReviewService.java
+++ Uber Project/UberReviewService/src/main/java/com/example/UberReviewService/service/ReviewService.java
@@ -1,17 +1,22 @@
-//package com.example.UberReviewService.service;
-//
-//import com.example.UberReviewService.models.Review;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//public interface ReviewService {
-//
-//    public Optional<Review> findReviewById(Long Id);
-//
-//    public List<Review> findAllReviews();
-//
-//    public boolean deleteReviewById(Long Id);
-//
-//    public Review publishReview(Review review);
-//}
+package com.example.UberReviewService.service;
+
+import com.example.UberReviewService.models.Review;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public interface ReviewService {
+
+    public Optional<Review> findReviewById(Long id);
+
+    public List<Review> findAllReviews();
+
+    public boolean deleteReviewById(Long id);
+
+    public Review publishReview(Review review);
+
+    public Review updateReview(Long id, Review review);
+
+}
--- Uber Project/UberReviewService/src/main/java/com/example/UberReviewService/service/ReviewServiceImpl.java
+++ Uber Project/UberReviewService/src/main/java/com/example/UberReviewService/service/ReviewServiceImpl.java
@@ -1,40 +1,73 @@
-//package com.example.UberReviewService.service;
-//
-//import com.example.UberReviewService.repositories.ReviewRepository;
-//import com.example.UberReviewService.models.Review;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//public class ReviewServiceImpl{
-//
-//    private ReviewRepository reviewRepository;
-//
-//    public ReviewServiceImpl(ReviewRepository reviewRepository){
-//        this.reviewRepository =reviewRepository;
-//    }
-//
-//
-//    @Override
-//    public Optional<Review> findReviewById(Long id) {
-//        return reviewRepository.findById(id);
-//    }
-//
-//    @Override
-//    public List<Review> findAllReviews() {
-//        return reviewRepository.findAll();
-//    }
-//
-//    @Override
-//    public boolean deleteReviewById(Long id) {
-//        try {
-//            reviewRepository.deleteById(id);
-//            return true;
-//        }catch (Exception e){
-//            return false;
-//        }
-//
-//    }
-//}
+package com.example.UberReviewService.service;
+
+import com.example.UberReviewService.models.Review;
+import com.example.UberReviewService.repositories.ReviewRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.hibernate.FetchNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+
+    private ReviewRepository reviewRepository;
+
+    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+
+    @Override
+    public Optional<Review> findReviewById(Long id) throws EntityNotFoundException {
+        Optional<Review> review;
+        try {
+            review = this.reviewRepository.findById(id);
+            if (review.isEmpty()) {
+                throw new EntityNotFoundException("Review with id " + id + " not found");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            if(e.getClass() == EntityNotFoundException.class){
+                throw new FetchNotFoundException("Review with id " + id + " not found", id);
+            }
+            throw new FetchNotFoundException("Unable to fetch, PLease try again later!", id);
+        }
+        return review;
+    }
+
+    @Override
+    public List<Review> findAllReviews() {
+        return this.reviewRepository.findAll();
+    }
+
+    @Override
+    public boolean deleteReviewById(Long id) {
+        try {
+            Review review = this.reviewRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+            this.reviewRepository.delete(review);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
+    @Override
+    public Review publishReview(Review review) {
+        return this.reviewRepository.save(review);
+    }
+
+    @Override
+    public Review updateReview(Long id, Review newReviewData) {
+        Review review = this.reviewRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        if(newReviewData.getRating() != null){
+            review.setRating(newReviewData.getRating());
+        }
+        if(newReviewData.getContent() != null){
+            review.setContent(newReviewData.getContent());
+        }
+        return this.reviewRepository.save(review);
+    }
+}
